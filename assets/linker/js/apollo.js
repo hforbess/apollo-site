ead0a537313e4038999a1e937a9f61a6cac4bde8
Harry Forbess <hforbess@gmail.com>
2014-05-29 01:50:31 +0000

    deleted swp files. put in ajax and loading gifs

--- assets/linker/js/apollo.js
+++ assets/linker/js/apollo.js
@@ -1,4 +1,5 @@
 $(function(){
+  $("#content").html("<div class='spinner'><img src='images/spiffy.gif' /> <h2 class='pull-right'>Loading...</h2> </div>" );
   $.ajax({
   url: "/sections/index",
   success:function( data ){
@@ -11,7 +12,7 @@ $(function(){
 
 $(function(){
  $('.change-content').click(function(){
-  $("#content").html("<div class='spinner'><img src='images/spiffy.gif' /> </div>" );
+  $("#content").html("<div class='spinner'><img src='images/spiffy.gif' />  <h2 class='pull-right' style='padding: 1em'>Loading...</h2></div>" );
   var section = this.id;
   $.ajax({
   url: "/sections/" + section,
